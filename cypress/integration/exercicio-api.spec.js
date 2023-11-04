9c22c65d1b454e19223a488e2b382996269d520b
vitoria <finkedificacoes@gmail.com>
2023-11-04 20:53:28 +0000

    buscando correlacoes no Postman

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -3,27 +3,66 @@
 describe('Testes da Funcionalidade Usuários', () => {
 
     it('Deve validar contrato de usuários', () => {
-         //TODO: 
+         //TODO: utilizar o joi
     });
 
     it('Deve listar usuários cadastrados', () => {
-         //TODO: 
+         //TODO: realizar um pedido GET 
+         cy.request({
+          method: 'GET',
+          url: 'usuarios'
+      }).then((response) => {
+          expect(response.status).to.equal(200)
+          expect(response.body).to.have.property('usuarios')
+          expect(response.duration).to.be.lessThan(20)
+      })
     });
 
     it('Deve cadastrar um usuário com sucesso', () => {
-         //TODO: 
+         //TODO: realizar um POST com o body completo e validar a assercao e frase de sucesso
+         cy.request({
+          method: 'POST',
+          url: 'usuarios',
+          body:
+          {
+               "nome": "joao Leonardo",
+               "email": "leodostccestes@qa.com.br",
+               "password": "teste",
+               "administrador": "true"
+          }
+      }).then((response) => {
+          expect(response.status).to.equal(201)
+          expect(response.body).to.have.property('usuarios')
+          expect(response.duration).to.be.lessThan(20)
+      })
+
     });
 
     it('Deve validar um usuário com email inválido', () => {
-         //TODO: 
+         //TODO: cadastrar usuario com email invalido e fazer a assercao da mensagem de erro e codigo de erro
+         cy.request({
+          method: 'POST',
+          url: 'usuarios',
+          body:
+          {
+               "nome": "joao Leonardo",
+               "email": "leodostccestes@qa.com.br",
+               "password": "teste",
+               "administrador": "true"
+          }
+      }).then((response) => {
+          expect(response.status).to.equal(400)
+          expect(response.body).to.have.property('usuarios')
+          expect(response.duration).to.be.lessThan(20)
+      })
     });
 
     it('Deve editar um usuário previamente cadastrado', () => {
-         //TODO: 
+         //TODO: realizar um cadastro, de seguida editar o mesmo usuario, utilizar o metodo PUT e validar a assercao com mensagem e codigo de sucesso
     });
 
     it('Deve deletar um usuário previamente cadastrado', () => {
-        //TODO: 
+        //TODO: criar um usuario, buscar id do usuario, utilizar DELETE, fazer assercao da mensagem de erro
     });
 
 
